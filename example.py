a3c6b8d7b502798655c656ed06505ea7cb3e4f1d
David Prelogovic <david.prelogovic@gmail.com>
2021-05-07 19:12:42 +0200

    small change

--- example.py
+++ example.py
@@ -9,4 +9,5 @@ data = np.random.normal(0, 1, size = (10, 100000)) * np.sqrt(np.arange(1, 11))[:
 
 pca.fit(data, batch_size = 2) # N_dim % batch_size == 0
 sampled_data = pca.sample(100000)
-print(np.std(pca.transform(sampled_data), axis = 1, ddof = 1)) # should be [10, 9, 8, 7, 6]
+print(np.std(pca.transform(sampled_data), axis = 1, ddof = 1)**2) # should be [10, 9, 8, 7, 6]
+print(pca.λ) # should be the same
