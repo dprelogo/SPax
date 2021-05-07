01412e140e33477d69698251c2ce1f186bfa54d7
David Prelogovic <david.prelogovic@gmail.com>
2021-05-07 18:05:43 +0200

    simple example

---
+++ example.py
@@ -0,0 +1,11 @@
+import jax
+jax.config.update("jax_platform_name", "cpu")
+import numpy as np
+import spax
+
+pca = spax.PCA(5, devices = jax.devices("gpu"))
+data = np.random.normal(0, 1, size = (100, 1000)) * np.arange(1, 101)
+
+pca.fit(data, batch_size = 10) # N_dims % batch_size == 0
+sampled_data = pca.sample(1000)
+print(np.std(sampled_data, axis = 1, ddof = 1)) # should be [100, 99, 98, 97, 96]
