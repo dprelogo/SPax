ad702226f7d4ee1b2a97e268492d9e6915e52ea6
David Prelogovic <david.prelogovic@gmail.com>
2021-05-07 19:02:40 +0200

    small change

--- example.py
+++ example.py
@@ -5,8 +5,8 @@ import numpy as np
 import spax
 
 pca = spax.PCA(5, devices = jax.devices("gpu"))
-data = np.random.normal(0, 1, size = (100, 1000)) * np.arange(1, 101)[:, np.newaxis]
+data = np.random.normal(0, 1, size = (10, 100000)) * np.sqrt(np.arange(1, 11))[:, np.newaxis]
 
-pca.fit(data, batch_size = 10) # N_dims % batch_size == 0
-sampled_data = pca.sample(1000)
-print(np.std(sampled_data, axis = 1, ddof = 1)) # should be [100, 99, 98, 97, 96]
+pca.fit(data, batch_size = 1000) # N_dims % batch_size == 0
+sampled_data = pca.sample(100000)
+print(np.std(pca.transform(sampled_data), axis = 1, ddof = 1)) # should be [100, 99, 98, 97, 96]
