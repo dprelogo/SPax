9f1ba6732ebc697f26b6e49b32943770506e12e9
David Prelogovic <david.prelogovic@gmail.com>
2021-05-11 17:59:31 +0200

    update example

--- example.py
+++ example.py
@@ -4,11 +4,11 @@ jax.config.update("jax_enable_x64", True)
 import numpy as np
 import spax
 
-pca = spax.PCA(5, devices = jax.devices("gpu"))
-data = np.random.normal(0, 1, size = (10, 100000)) * np.sqrt(np.arange(1, 11))[:, np.newaxis]
+pca = spax.PCA_m(5, devices = jax.devices("gpu"))
+data = np.random.normal(0, 1, size = (16, 100000)) * np.sqrt(np.arange(1, 17))[:, np.newaxis]
 
-pca.fit(data, batch_size = 2) # N_dim % batch_size == 0
+pca.fit(data, batch_size = 2) # N_dim % (N_devices * batch_size) == 0
 sampled_data = pca.sample(100000)
-print(np.std(pca.transform(sampled_data), axis = 1, ddof = 1)**2) # should be [6, 7, 8, 9, 10]
+print(np.std(pca.transform(sampled_data), axis = 1, ddof = 1)**2) # should be [12, 13, 14, 15, 16]
 print(pca.λ) # should be the same
 print(np.round(pca.v.T, 1)) # should be a +-unit matrix on last 5 dimensions
