38ab28ad19cb048e3406a012e96a6dd91f8d37f1
David Prelogovic <david.prelogovic@gmail.com>
2021-07-27 12:23:26 +0200

    write kernels with einsum

--- spax/kernels.py
+++ spax/kernels.py
@@ -7,25 +7,34 @@ import jax.numpy as jnp
 #         raise ValueError("x and y shapes are not the same")
 
 @jax.jit
-def _rbf(x, y, gamma = 1.):
-    d = x - y
-    return jnp.exp(-gamma * d.T @ d)
+def _linear(x, y):
+    return jnp.einsum("ij,ik->jk", x, y)
 
 @jax.jit
-def _linear(x, y):
-    return x.T @ y
+def _rbf(x, y, gamma = 1.):
+    # if jnp.ndim(x) == 1:
+    #     x = x.reshape(-1, 1)
+    #     y = y.reshape(-1, 1)
+    xx = jnp.einsum("ik,ik->k", x, x).reshape(-1, 1)
+    yy = jnp.einsum("ik,ik->k", y, y).reshape(1, -1)
+    xy = _linear(x, y)
+    d2 = xx + yy - 2 * xy
+    return jnp.exp(-gamma * d2)
 
 @jax.jit
 def _poly(x, y, gamma = 1., r = 1., d = 2.):
-    return jnp.power(gamma * x.T @ y + 1, d)
+    return jnp.power(gamma * _linear(x, y) + r, d)
 
 @jax.jit
 def _tanh(x, y, gamma = 1., r = 1.): 
-    return jnp.tanh(gamma * x.T @ y + r) # often called `sigmoid`
+    return jnp.tanh(gamma * _linear(x, y) + r) # often called `sigmoid`
 
 @jax.jit
 def _cosine(x, y):
     '''
     Computing cosine similarity between x & y.
     '''
-    return x.T @ y / jnp.sqrt(x.T @ x * y.T @ y)
+    xx = jnp.einsum("ik,ik->k", x, x).reshape(-1, 1)
+    yy = jnp.einsum("ik,ik->k", y, y).reshape(1, -1)
+    xy = _linear(x, y)
+    return xy / (xx @ yy)
