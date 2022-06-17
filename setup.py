4d885a1d469aab1f9e7d30ea959902c99ffe7db6
David Prelogovic <david.prelogovic@gmail.com>
2022-06-17 19:02:33 +0200

    update jaxlib version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 with open("README.md", "r") as f:
     long_description = f.read()
-    
-_current_jaxlib_version = '0.3.5'
-_default_cuda_version = '11'
-_default_cudnn_version = '82'
+
+_current_jaxlib_version = "0.3.10"
+_default_cuda_version = "11"
+_default_cudnn_version = "805"
 
 setup(
     name="SPax",
@@ -21,18 +21,17 @@ setup(
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        ],
+    ],
     python_requires=">=3.7",
     install_requires=[
         "jax",
         "numpy",
         "h5py",
-        ],
+    ],
     extras_require={
         "cpu": [f"jaxlib=={_current_jaxlib_version}"],
         "gpu": [
             f"jaxlib=={_current_jaxlib_version}+cuda{_default_cuda_version}.cudnn{_default_cudnn_version}"
         ],
-        
     },
 )
